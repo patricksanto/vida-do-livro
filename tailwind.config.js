1dc0f320813940e782c4ad807d459e44ef459c0b
Patrick Santos <patrick.sg.santos@gmail.com>
2023-06-07 09:12:45 -0700

    fonts installed, and first screen done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,19 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'  ],
+    './app/javascript/**/*.js'
+  ],
   theme: {
     extend: {
       fontFamily: {
-        'nb': ['Nb', 'sans-serif'],
-        'alt': ['Cheltenham', 'arial', 'serif'],
+        'nb': ['Nb International'],
+        'alt': ['Cheltenhm BdCn BT', 'arial', 'serif'],
+        'greating': ['Greating', 'sans-serif'],
       },
       colors: {
         'primary': '#EFEDDF',
         'secondary': '#C4D6C9',
       },
-    },
+    }
   }
 }
